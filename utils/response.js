f8d76533b6666610c14aa4496c3c9703a013a7d5
ledinhchinh0212 <khongbachofficial@gmail.com>
2025-06-09 02:34:42 +0700

    Refactor RoleResponse function to support multiple command names and improve message matching

--- utils/response.js
+++ utils/response.js
@@ -1,14 +1,26 @@
 const EmbedBuilderWerewolf = require('./embed');
 const roles = require('../data/data.json');
 
+/**
+ * Tạo embed theo từng role
+ *
+ * @param {import('discord.js').Message} message - Tin nhắn nhận được từ người dùng
+ * @param {string[]} commandNames - 1 mảng các commands vd: ['!danlang', '!villager']
+ * @param {string} fileName - Tên file
+ * @param {number} indexRole - Role id
+ * @param {string} factionRole - 'Dân làng', 'Ma sói'
+ * @returns {Promise<void>}
+ */
 const RoleResponse = async (
   message,
-  commandName,
+  commandNames,
   fileName,
   indexRole,
   factionRole
 ) => {
-  if (message.content === commandName) {
+  const msg = message.content.toLowerCase();
+
+  if (commandNames.map(cmd => cmd.toLowerCase()).includes(msg)) {
     const { embed, file } = EmbedBuilderWerewolf(fileName, {
       title: `${roles[indexRole].title} (${roles[indexRole].eName})`,
       description: `${roles[indexRole].description}\n\nPhe: ${factionRole}`,
