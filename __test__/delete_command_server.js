1a03d8521ec3c3959bf7424565bb15b8c4c98855
ledinhchinh0212 <khongbachofficial@gmail.com>
2025-06-09 17:07:05 +0700

    Add script to delete all slash commands in a guild

---
+++ __test__/delete_command_server.js
@@ -0,0 +1,20 @@
+const { REST, Routes } = require('discord.js');
+require('dotenv').config();
+
+const token = process.env.TOKEN;
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Đang xoá tất cả slash command trong guild...');
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      body: [],
+    });
+    console.log('Đã xoá toàn bộ slash command trong guild!');
+  } catch (error) {
+    console.error('Lỗi khi xoá command:', error);
+  }
+})();
