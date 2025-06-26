b6efa5aee81696240655cbfc150f63637ac0475f
ledinhchinh0212 <khongbachofficial@gmail.com>
2025-06-26 13:17:48 +0700

    refactor: simplify message handling by integrating commandHandler and streamline database connection

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { RoleResponse } = require('./utils/response');
 const { FactionRole } = require('./types/faction');
-const { store, serverSettings } = require('./core/store');
+const { store } = require('./core/store');
 const { gameRooms } = require('./core/room');
 const { WEREROLE } = require('./utils/role');
 const Dead = require('./types/roles/Dead');
 const ServerSettings = require('./models/ServerSettings');
 const connectDB = require('./config/database');
-
+const commandHandler = require('./src/client/handlers/commandHandler');
 require('dotenv').config();
 
 connectDB();
@@ -65,128 +65,8 @@ client.once('ready', () => {
 });
 
 client.on('messageCreate', async (message) => {
-  // if (message.author.bot) return;
+  commandHandler(message);
 
-  await RoleResponse(
-    message,
-    ['!soi', '!masoi', '!werewolf'],
-    'werewolf.png',
-    0,
-    FactionRole.Werewolf
-  );
-  await RoleResponse(
-    message,
-    ['!danlang', '!villager'],
-    'villager.png',
-    1,
-    FactionRole.Village
-  );
-  await RoleResponse(
-    message,
-    ['!baove', '!bodyguard'],
-    'bodyguard.png',
-    2,
-    FactionRole.Village
-  );
-  await RoleResponse(
-    message,
-    ['!bansoi', '!cursed'],
-    'cursed.png',
-    3,
-    FactionRole['Vi-Wolf']
-  );
-  await RoleResponse(
-    message,
-    ['!tientri', '!seer'],
-    'seer.png',
-    4,
-    FactionRole.Village
-  );
-  await RoleResponse(
-    message,
-    ['!thamtu', '!detective'],
-    'detective.png',
-    5,
-    FactionRole.Village
-  );
-  await RoleResponse(
-    message,
-    ['!phuthuy', '!witch'],
-    'witch.png',
-    6,
-    FactionRole.Village
-  );
-  await RoleResponse(
-    message,
-    ['!thangngo', '!fool'],
-    'fool.png',
-    7,
-    FactionRole.Solo
-  );
-  await RoleResponse(
-    message,
-    ['!thaydong', '!medium'],
-    'medium.png',
-    8,
-    FactionRole.Village
-  );
-  await RoleResponse(
-    message,
-    ['!haugai', '!maid'],
-    'maid.png',
-    10,
-    FactionRole.Village
-  );
-  await RoleResponse(message, ['!lycan'], 'lycan.png', 11, FactionRole.Village);
-  await RoleResponse(
-    message,
-    ['!stalker', '!hori', '!stalkẻ'],
-    'stalker.png',
-    16,
-    FactionRole.Solo
-  );
-  await RoleResponse(
-    message,
-    ['!wolfseer', '!soitientri', '!soitri'],
-    'wolf_seer.png',
-    12,
-    FactionRole.Werewolf
-  );
-  await RoleResponse(
-    message,
-    ['!alphawerewolf', '!soitrum', '!soicosplay'],
-    'alpha_werewolf.png',
-    13,
-    FactionRole.Werewolf
-  );
-  await RoleResponse(
-    message,
-    ['!cao', '!foxspirit', '!holy', '!fox'],
-    'fox_spirit.png',
-    14,
-    FactionRole.Village
-  );
-  await RoleResponse(
-    message,
-    ['!gialang', '!elder'],
-    'elder.png',
-    15,
-    FactionRole.Village
-  );
-  await RoleResponse(
-    message,
-    ['!xathu', '!gunner'],
-    'gunner.png',
-    17,
-    FactionRole.Village
-  );
-  await RoleResponse(
-    message,
-    ['!soimeocon', '!kittenwolf'],
-    'kitten_wolf.png',
-    18,
-    FactionRole.Werewolf
-  );
   if (message.channel.type === ChannelType.DM) {
     console.log(`Tin nhắn DM từ ${message.author.tag}: ${message.content}`);
 
